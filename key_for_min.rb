5cd7bb0b86edc46ccd5a0572d14c70788efac97e
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-04-18 01:54:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  return nil if name_hash.empty?
-  name_hash.max_by {|k, v| 0-v}[0]
-end
+#def key_for_min_value(name_hash)
+#  return nil if name_hash.empty?
+#  name_hash.max_by {|k, v| 0-v}[0]
+#end
 
+def key_for_min_value(name)
+  
