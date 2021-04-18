da48f1f9950c259f8ca30f6171bd712a77e3dcf9
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-04-18 02:36:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,11 +6,4 @@ def key_for_min_value(name_hash)
   name_hash.max_by {|k, v| 0-v}[0]
 end
 
-#def key_for_min_value(name)
- # return nil if name.empty?
- # name.collect do |k,v| 
- #   if v <=> v
- #     puts #{k}
- #   end 
- # end
-#end 
+
